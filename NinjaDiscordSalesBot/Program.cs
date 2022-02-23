9a6715e7cba07db302769f4d1b34f3c57cc91c76
Cristi Pufu <cristian.pufu@uipath.com>
2022-02-23 12:44:29 +0200

    add dependabot

--- NinjaDiscordSalesBot/Program.cs
+++ NinjaDiscordSalesBot/Program.cs
@@ -2,10 +2,10 @@
 
 var bot = new NinjaBot(new NinjaBotOptions
 {
-    DiscordBotToken = "<discord_bot_token>",
-    DiscordChannelId = "<discord_channel_id>",
-    OpenSeaApiKey = "<opensea_api_key>",
-    OpenSeaCollectionSlug = "<collection_slug>",
+    DiscordBotToken = "OTM4NzUxMDI0ODkyNjI5MDIy.Yfu2BQ.O3P-a9a8I5-XCc3vcFLCPkxLKVw",
+    DiscordChannelId = "935635694490116156",
+    OpenSeaApiKey = "0057a183991745f6bbdb385493edbf10",
+    OpenSeaCollectionSlug = "planetdaos",
     PollingIntervalSeconds = 60000,
 });
 
